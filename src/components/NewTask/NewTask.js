dfa0690b5c91d13f7e21b152a3308d194b66aad5
Samuel <samkoj@hotmail.co.uk>
2021-07-31 08:53:18 +0100

    update NewTask component to use custom useHttp hook to send POST request

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -2,39 +2,31 @@ import { useState } from "react";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
+import useHttp from "../../hooks/use-http";
 
 const NewTask = (props) => {
-	const [isLoading, setIsLoading] = useState(false);
-	const [error, setError] = useState(null);
+	const { isLoading, error, sendRequest } = useHttp();
 
+	const createTask = (taskText, taskData) => {
+		const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+		const createdTask = { id: generatedId, text: taskText };
+		props.onAddTask(createdTask);
+	};
+
+	// bind the createTask function with taskText object to make it accessible. (Alternative to nesting createTask inside of enterTaskHandler)
 	const enterTaskHandler = async (taskText) => {
-		setIsLoading(true);
-		setError(null);
-		try {
-			const response = await fetch("https://max-udemy-s15-custom-hooks-default-rtdb.firebaseio.com/tasks.json", {
+		sendRequest(
+			createTask.bind(null, taskText),
+			"https://max-udemy-s15-custom-hooks-default-rtdb.firebaseio.com/tasks.json",
+			{
 				method: "POST",
 				body: JSON.stringify({ text: taskText }),
 				headers: {
 					"Content-Type": "application/json",
 				},
-			});
-
-			if (!response.ok) {
-				throw new Error("Request failed!");
 			}
-
-			const data = await response.json();
-
-			const generatedId = data.name; // firebase-specific => "name" contains generated id
-			const createdTask = { id: generatedId, text: taskText };
-
-			props.onAddTask(createdTask);
-		} catch (err) {
-			setError(err.message || "Something went wrong!");
-		}
-		setIsLoading(false);
+		);
 	};
-
 	return (
 		<Section>
 			<TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
